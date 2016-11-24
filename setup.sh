6eeeb168364e4c5383f7a49331d8ea206ab519a8
Fred Campos <mail2frede@gmail.com>
2016-11-24 16:43:17 -0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,17 +4,13 @@
 # Script for installing my env apps
 #
 
+DISTRO="ubuntu-xenial"
 USER_NAME="$(whoami)"
 HOME_PATH="/home/${USER_NAME}"
-
-DISTRO="ubuntu-xenial"
-
-VAGRANT_VERSION="1.8.4"
+VAGRANT_VERSION="1.8.7"
 KERNEL="$(uname -s)"
 ARCH="$(uname -m)"
-NODE_VERSION="6.2.2"
-PHP_VERSION="7.0"
-
+CURL_VERSION="7.51.0"
 TIMEZONE="America/Sao_Paulo"
 
 # utils
@@ -26,15 +22,13 @@ sudo apt-get install -y aptitude  \
     automake \
     build-essential \
     libssl-dev \
-    gedit \
     git \
-    curl \
     htop \
     iotop \
     vim \
-    meld \
     libreoffice \
     tmux \
+    tmuxinator \
     xclip \
     subversion \
     midori \
@@ -61,9 +55,8 @@ sudo apt-get install -y aptitude  \
     galculator \ 
     chromium \ 
     flashplugin-installer \
-    libmongo-client-dev \
-    libpq-dev \
-    xchm 
+    xchm \
+    wget
 echo "<< installing some utilities and deps  [end]"
 
 # vim spf-13
@@ -79,31 +72,21 @@ sudo apt-get install -y sqlite3 \
 echo "<< installing db clients [end]"
 
 # shell
+sudo apt-get install shellcheck
 if [ ! -f "$(command which zsh)" ]; then
-    echo "<< installing zsh & shell tools"
-    sudo apt-get install -y zsh shellcheck
+    echo "<< installing zsh"
+    sudo apt-get install -y zsh
     echo "<< changing shell, maybe it will ask password"
     chsh -s /bin/zsh
     /bin/zsh
-    echo "<< installing zsh & shell tools [end]"
-fi
-
-# node
-if [ ! -d /opt/node ] || [ ! -f "$(command which npm)" ]; then
-    echo "<< installing nodejs"
-    wget -O "/tmp/node-v${NODE_VERSION}-linux-x64.tar.gz" "http://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.gz"
-    sudo tar -zxvf "/tmp/node-v${NODE_VERSION}-linux-x64.tar.gz" -C /opt
-    sudo mv "/opt/node-v${NODE_VERSION}-linux-x64" /opt/node
-    sudo su -c "/opt/node/bin/npm config set python /usr/bin/python2 -g"
-    sudo su -c "/opt/node/bin/npm install -g jshint"
-    echo "<< installing nodejs [end]"
+    echo "<< installing zsh [end]"
 fi
 
 # python
 echo "<< installing python & tools"
-sudo apt-get install -y python-dev python-pip
-sudo pip install --upgrade pip
-sudo pip install pylama pylama_pylint flake8-import-order jedi autopep8 virtualenvwrapper
+sudo apt-get install -y python3-dev python3-pip
+sudo pip3 install --upgrade pip
+sudo pip3 install pylama pylama_pylint pylama_gjslint jedi autopep8 virtualenvwrapper radon isort
 echo "<< installing python & tools [end]"
 
 # c/cpp
@@ -111,45 +94,10 @@ echo "<< installing clang"
 sudo apt-get install -y clang uncrustify
 echo "<< installing clang [end]"
 
-#ruby
-echo "<< installing ruby"
-sudo apt-get install -y ruby ruby-dev rubygems-integration
-sudo gem install rubocop bundler tmuxinator fpm
-echo "<< installing ruby [end]"
-
 #go
 echo "<< installing goLang"
 sudo apt-get install -y golang golang-go.tools
 echo "<< installing goLang [end]"
-
-#php
-echo "<< installing php & tools"
-sudo apt-get install -y "php${PHP_VERSION}-cli php${PHP_VERSION}-dev php${PHP_VERSION}-curl php${PHP_VERSION}-intl"
-if [ ! -f /usr/local/bin/composer ]; then
-    curl -sS https://getcomposer.org/installer | php
-    chmod +x "${PWD}/composer.phar" ; sudo mv "${PWD}/composer.phar" /usr/local/bin/composer
-fi
-if [ ! -f /usr/local/bin/phpcs ]; then
-    sudo wget -O /usr/local/bin/phpcs https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
-    sudo chmod +x /usr/local/bin/phpcs
-fi
-if [ ! -f /usr/local/bin/phpcbf ]; then
-    sudo wget -O /usr/local/bin/phpcbf https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
-    sudo chmod +x /usr/local/bin/phpcbf
-fi
-if [ ! -f /usr/local/bin/php-cs-fixer ]; then
-    sudo wget -O /usr/local/bin/php-cs-fixer http://get.sensiolabs.org/php-cs-fixer.phar
-    sudo chmod +x /usr/local/bin/php-cs-fixer
-fi
-if [ ! -f /usr/local/bin/phpmd ]; then
-    sudo wget -O /usr/local/bin/phpmd http://static.phpmd.org/php/latest/phpmd.phar
-    sudo chmod +x /usr/local/bin/phpmd
-fi
-if [ ! -f /usr/local/bin/phpdox ]; then
-    sudo wget -O /usr/local/bin/phpdox http://phpdox.de/releases/phpdox.phar
-    sudo chmod +x /usr/local/bin/phpdox
-fi
-echo "<< installing php & tools [end]"
 
 # atom
 if [ ! -f "$(which atom)" ]; then
@@ -161,32 +109,24 @@ if [ ! -f "$(which atom)" ]; then
         echo "<< [error] apm not found"
         exit 0
     fi
-    apm install atom-beautify \
-    autocomplete-paths \
-    autocomplete-plus-python-jedi \
-    autocomplete-php \
-    autocomplete-phpunit \
-    autocomplete-clang \
-    go-plus \
-    dockblockr \
-    language-docker \
-    linter \
-    linter-jshint \
-    linter-php \
-    linter-phpcs \
-    linter-phpmd \
-    linter-shellcheck \
-    linter-flake8 \
-    linter-golinter \
-    linter-gotype \
-    linter-govet \
-    linter-rubocop \
-    linter-ruby \
-    linter-clang \
-    script \
-    emmet \
-    minimap \
-    language-x86
+    apm install atom-beautify
+    apm install autocomplete-paths
+    apm install autocomplete-python
+    apm install autocomplete-clang
+    apm install atom-jinja2
+    apm install go-plus
+    apm install dockblockr
+    apm install language-docker
+    apm install linter
+    apm install linter-shellcheck
+    apm install linter-pylama
+    apm install linter-clang
+    apm install go-plus
+    apm install minimap
+    apm install language-x86
+    apm install language-r
+    apm install autocomplete-R
+    apm install linter-lintr
     echo "<< installing atom editor & plugins [end]"
 fi
 
@@ -211,6 +151,16 @@ if [ ! -f "$(which vagrant)" ]; then
     }
     echo "<< installing vagrant & virtualbox [end]"
 fi
+
+#curl
+echo "<< installing curl"
+sudo apt-get build-dep -y curl
+git clone https://github.com/tatsuhiro-t/nghttp2.git ~/Downloads && cd ~/Downloads/nghttp2
+autoreconf -i && automake && autoconf && ./configure && make && sudo make install && cd ~/Downloads
+wget http://curl.haxx.se/download/curl-${CURL_VERSION}.tar.bz2 && tar -jxvf curl-${CURL_VERSION}.tar.bz2
+cd curl-${CURL_VERSION} && ./configure --with-nghttp2=/usr/local --with-ssl && make && sudo make install
+sudo ldconfig
+echo "<< installing curl [end]"
 
 echo "<< cleaning and removing old packages "
 sudo apt-get autoremove -y
